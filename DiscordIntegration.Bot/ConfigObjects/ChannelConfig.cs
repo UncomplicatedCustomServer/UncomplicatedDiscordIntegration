78301418748c65ca9236f79d7b5ee7f8f9b80661
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2024-09-08 21:11:01 +0000

    removed everything - useless

--- DiscordIntegration.Bot/ConfigObjects/ChannelConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace DiscordIntegration.Bot.ConfigObjects;
-
-public class ChannelConfig
-{
-    public LogChannels Logs { get; set; } = new();
-    public List<ulong> TopicInfo { get; set; } = new();
-    public List<ulong> CommandChannel { get; set; } = new();
-}
--- DiscordIntegration.Bot/ConfigObjects/LogType.cs
+++
@@ -1,7 +0,0 @@
-namespace DiscordIntegration.Bot.ConfigObjects;
-
-public enum LogType
-{
-    Embed,
-    Text,
-}
